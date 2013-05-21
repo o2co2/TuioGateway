7915063261e8794ca9e20dbb4e269db5040e3e34
vinjn <vinjn.z@gmail.com>
2013-05-22 00:13:41 +0800

    update code

--- src/TuioGateApp.cpp
+++ src/TuioGateApp.cpp
@@ -1,155 +1,324 @@
-/*
- Copyright (c) 2010, Hector Sanchez-Pajares
- Aer Studio http://www.aerstudio.com
- All rights reserved.
- 
- 
- This is a block for TUIO Integration for the Cinder framework (http://libcinder.org)
- 
- Redistribution and use in source and binary forms, with or without modification, are permitted provided that
- the following conditions are met:
- 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and
- the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
- the following disclaimer in the documentation and/or other materials provided with the distribution.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
- */
-
+#include "cinder/Cinder.h"
 #include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include "cinder/System.h"
+#include "cinder/Rand.h"
+#include "cinder/Utilities.h"
+#include "cinder/tuio/TuioClient.h"
+#include "cinder/osc/OscSender.h"
+#include "cinder/params/Params.h"
+#include "MiniConfig.h"
 
 using namespace ci;
 using namespace ci::app;
+
+#include <vector>
+#include <map>
+#include <list>
 using namespace std;
 
-#include "TuioClient.h"
-#include "TuioCursor.h"
-#include "OscMessage.h"
-#include "MiniConfig.h"
+class TuioGateway : public AppBasic {
+public:
+//     void prepareSettings( Settings *settings )
+//     {
+//         settings->enableMultiTouch();
+//     }
 
-class TuioClientApp : public AppBasic {
- public:
-	void setup();
-	void update();
-	void draw();
-	
-	void cursorAdded( tuio::Cursor cursor );
-	void cursorUpdated( tuio::Cursor cursor );
-	void cursorRemoved( tuio::Cursor cursor );
-	void oscMessage( const osc::Message *message );
+    void mouseDown( MouseEvent event )
+    {
+        mouseDrag(event);
+        mPrevCursorPos = mCursorPos;
+    }
 
-	tuio::Client tuio;
+    void mouseUp( MouseEvent event )
+    {
+        mCursorPressed = false;
+    }
 
-	CallbackId	mUpdatedCallbackIndex;
+    void mouseDrag( MouseEvent event )
+    {
+        mCursorPressed = true;
+        mCursorPos = event.getPos();
+    }
+
+    enum {USAGE_CLIENT, USAGE_SERVER, USAGE_GATEWAY, USAGE_COUNT};
+
+    void onConnect()
+    {
+        APP_USAGE = math<int>::clamp(APP_USAGE, USAGE_CLIENT, USAGE_GATEWAY);
+
+        mCursorPressed = false;
+
+        mTuioClient.disconnect();
+        mTuioServer = osc::Sender();
+        mOscServer = osc::Sender();
+
+        char buffer[MAX_PATH] = {0};
+
+        strcpy(buffer, mEnumTypes[APP_USAGE].c_str());
+
+#define CATCH_ERROR  catch (std::exception& e)   {\
+            sprintf_s(buffer, MAX_PATH, "%s | [FAIL] %s!!!", buffer, e.what());\
+            console() << __FILE__ << " (" << __LINE__ << ") " << e.what() << endl;\
+            mCurrentAppUsage = USAGE_COUNT;\
+            break;}
+
+        do 
+        {
+            if (APP_USAGE != USAGE_SERVER)
+            {
+                try
+                {
+                    mTuioClient.connect(LOCAL_TUIO_PORT);
+                    sprintf_s(buffer, MAX_PATH, "%s | listen at #%d", 
+                        buffer, LOCAL_TUIO_PORT);
+                    mCurrentAppUsage = APP_USAGE;
+                }
+                CATCH_ERROR 
+            }
+
+            if (APP_USAGE != USAGE_CLIENT)
+            {
+                try
+                {
+                    mTuioServer.setup(REMOTE_IP, REMOTE_TUIO_PORT);
+                    mOscServer.setup(REMOTE_IP, REMOTE_OSC_PORT);
+                    sprintf_s(buffer, MAX_PATH , "%s | sending to %s: #%d | #%d", 
+                        buffer, REMOTE_IP.c_str(), REMOTE_TUIO_PORT, REMOTE_OSC_PORT);
+                    mCurrentAppUsage = APP_USAGE;
+                }
+                CATCH_ERROR
+            }
+        } while (0);
+
+#undef CATCH_ERROR
+
+        mStatus = buffer;
+    }
+
+    void setup()
+    {
+        console() << "TuioGateway built on " << __DATE__ << endl;
+
+        readConfig();
+        mStatus = "idle..press CONNECT button";
+        mCurrentAppUsage = USAGE_COUNT;
+
+        mParams = params::InterfaceGl("param", Vec2i(270, 240));
+        {
+            mEnumTypes.clear();
+            mEnumTypes.push_back("Client");
+            mEnumTypes.push_back("Server");
+            mEnumTypes.push_back("Router");
+
+            // MAGIC!
+            int lines = 0;
+#define ITEM_DEF(type, var, default) mParams.addParam(#var, &var);
+#include "item.def"
+#undef ITEM_DEF
+
+            // HACK!
+            mParams.removeParam("APP_USAGE");
+            mParams.addSeparator();
+            mParams.addParam("APP_USAGE", mEnumTypes, &APP_USAGE);
+            mParams.addSeparator();
+            mParams.addButton("CONNECT", std::bind(&TuioGateway::onConnect, this));
+            mParams.addButton("SAVE", writeConfig);
+
+            onConnect();
+        }
+  
+        //mTuioClient.registerTouches( this );
+        //console() << "MT: " << System::hasMultiTouch() << " Max points: " << System::getMaxMultiTouchPoints() << std::endl;
+
+        mFont = Font("YouYuan", 22);
+    }
+
+    void update()
+    {
+        N_DISPLAYS = math<int>::clamp(N_DISPLAYS, 1, 8);
+        REMOTE_DISPLAY_ID = math<int>::clamp(REMOTE_DISPLAY_ID, 1, N_DISPLAYS);
+
+        mActiveTouches = mTuioClient.getCursors();
+
+        if (mCursorPressed)
+        {
+            mActiveTouches.push_back(tuio::Cursor(
+                "", -1, Vec2f(mCursorPos.x/getWindowWidth(), mCursorPos.y/getWindowHeight())
+                ));
+            mPrevCursorPos = mCursorPos;
+        }
+
+        if (mCurrentAppUsage == USAGE_SERVER || mCurrentAppUsage == USAGE_GATEWAY)
+        {
+            sendTuioMessage(mTuioServer, mActiveTouches);
+            sendOscMessages(mOscServer, mActiveTouches);
+        }
+    }
+
+    void draw()
+    {
+        gl::enableAlphaBlending();
+        gl::clear( Color( 0.1f, 0.1f, 0.1f ) );
+        gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
+
+        if (VISUAL_EFFECT)
+        {
+            // draw yellow circles at the active touch points
+            gl::color( Color( 1, 1, 0 ) );
+
+            for( vector<tuio::Cursor>::const_iterator touchIt = mActiveTouches.begin(); touchIt != mActiveTouches.end(); ++touchIt )
+                gl::drawStrokedCircle( Vec2f(touchIt->getPos().x * getWindowWidth(), touchIt->getPos().y * getWindowHeight()),20.0f );
+
+            gl::drawString(mStatus, Vec2f(10, getWindowHeight() - 100), ColorA::white(), mFont);
+        }
+
+        mParams.draw();
+    }
+
+    void keyDown( KeyEvent event ) {
+        switch (event.getCode())
+        {
+//         case KeyEvent::KEY_SPACE:
+//             {
+//                 setFullScreen( ! isFullScreen() ); 
+//             }break;
+        case KeyEvent::KEY_ESCAPE:
+            {
+                quit();
+            }break;
+        case KeyEvent::KEY_h:
+            {
+                if (mParams.isVisible())
+                {
+                    mParams.show( false );
+                    VISUAL_EFFECT = false;
+                }
+                else
+                {
+                    mParams.show( true );
+                    VISUAL_EFFECT = true;
+                }
+            }
+        default:
+            break;
+        }
+    }
+
+    void sendTuioMessage( osc::Sender& sender, const vector<tuio::Cursor>& cursors ) 
+    {
+        osc::Bundle b;
+        osc::Message alive;
+        // Sends alive message - saying 'Hey, there's no alive blobs'
+        alive.setAddress("/tuio/2Dcur");
+        alive.addStringArg("alive");
+
+        // Send fseq message
+        osc::Message fseq;
+        fseq.setAddress( "/tuio/2Dcur" );
+        fseq.addStringArg( "fseq" );
+        fseq.addIntArg(getElapsedFrames());
+
+        if (!cursors.empty())
+        {
+            float cellSize = 1.0f/N_DISPLAYS;
+            float x0 = cellSize * (REMOTE_DISPLAY_ID - 1);
+            float x1 = cellSize * (REMOTE_DISPLAY_ID);
+
+            for (vector<tuio::Cursor>::const_iterator it = cursors.begin(); it != cursors.end(); ++it)
+            {
+                float x = it->getPos().x;
+                float y = it->getPos().y;
+
+                x = x0 + x * cellSize;
+                //y = y0 + y * cellSize;
+
+                if (x <= x0 || x >= x1 || y <= 0 || y >= 1.0f)
+                    continue;
+
+                osc::Message set;
+                set.setAddress( "/tuio/2Dcur" );
+                set.addStringArg("set");
+                set.addIntArg(it->getSessionId());				// id
+                set.addFloatArg(x);	// x
+                set.addFloatArg(y);	// y
+                set.addFloatArg(it->getSpeed().x);			// dX
+                set.addFloatArg(it->getSpeed().y);			// dY
+                set.addFloatArg(it->getMotionAccel());		// m
+
+                b.addMessage( set );							// add message to bundle
+                alive.addIntArg(it->getSessionId());				// add blob to list of ALL active IDs
+            }
+        }
+        b.addMessage( alive );		// add message to bundle
+        b.addMessage( fseq );		// add message to bundle
+
+        try
+        {
+            sender.sendBundle( b ); // send bundle
+        }
+        catch (std::exception& e)
+        {
+            console() << "sendBundle : " << e.what() << endl;
+        }
+    }
+
+    void sendOscMessages( osc::Sender& sender, const vector<tuio::Cursor>& cursors ) 
+    {
+        if (!cursors.empty())
+        {
+            float cellSize = 1.0f/N_DISPLAYS;
+            float x0 = cellSize * (REMOTE_DISPLAY_ID - 1);
+            float x1 = cellSize * (REMOTE_DISPLAY_ID);
+
+            for (vector<tuio::Cursor>::const_iterator it = cursors.begin(); it != cursors.end(); ++it)
+            {
+                float x = it->getPos().x;
+                float y = it->getPos().y;
+
+                x = x0 + x * cellSize;
+                //y = y0 + y * cellSize;
+
+                if (x <= x0 || x >= x1 || y <= 0 || y >= 1.0f)
+                    continue;
+
+                osc::Bundle bundle;
+                {
+                    osc::Message m;
+                    m.setAddress("/cursor/x");
+                    m.addFloatArg(x);
+                    bundle.addMessage(m);
+                }
+                {
+                    osc::Message m;
+                    m.setAddress("/cursor/y");
+                    m.addFloatArg(y);
+                    bundle.addMessage(m);
+                }
+                sender.sendBundle(bundle);
+                break;// only send first valid cursor
+            }
+        }
+    }
+
+private:
+    params::InterfaceGl         mParams;
+
+    tuio::Client				mTuioClient;
+    osc::Sender                 mTuioServer;
+    osc::Sender                 mOscServer;
+
+    vector<string>              mEnumTypes;
+    string                      mStatus;
+    Font                        mFont;
+
+    mutex                       mMutex;
+    bool                        mCursorPressed;
+    Vec2f                       mCursorPos, mPrevCursorPos;
+
+    int                         mCurrentAppUsage;
+    vector<tuio::Cursor>        mActiveTouches;
 };
 
+CINDER_APP_BASIC( TuioGateway, RendererGl )
 
-void TuioClientApp::setup()
-{
-    readConfig();
-
-	tuio.connect( 3333 );
-	
-	tuio.registerCursorAdded( this, &TuioClientApp::cursorAdded );
-	mUpdatedCallbackIndex = tuio.registerCursorUpdated( this, &TuioClientApp::cursorUpdated );
-	tuio.registerCursorRemoved( this, &TuioClientApp::cursorRemoved );
-	tuio.registerOscMessageReceived( this, &TuioClientApp::oscMessage );
-}
-
-void TuioClientApp::cursorAdded( tuio::Cursor cursor ) {
-	console() << "Cursor added " << cursor.getSessionId() << std::endl;
-}
-
-void TuioClientApp::cursorUpdated( tuio::Cursor cursor ) {
-	console() << "Cursor updated " << cursor.getSessionId() << std::endl;
-}
-
-void TuioClientApp::cursorRemoved( tuio::Cursor cursor ) {
-	console() << "Cursor removed " << cursor.getSessionId() << std::endl;
-}
-
-void TuioClientApp::oscMessage( const osc::Message *message) {
-	console() << "OSC Message " << message->getAddress() << std::endl;
-}
-
-void TuioClientApp::update()
-{
-	if( getElapsedSeconds() > 10.0f && mUpdatedCallbackIndex > -1 ) {
-		console() << "Unregistering cursor updated." << std::endl;
-		tuio.unregisterCursorUpdated( mUpdatedCallbackIndex );
-		mUpdatedCallbackIndex = -1;
-	}
-}
-
-void draw2( tuio::Cursor cursor, int sourcenum )
-{	
-	switch (sourcenum%6) {
-	case 0: gl::color(ColorA(1.0f, 0.0f, 0.0f, 0.6f)); break;
-	case 1: gl::color(ColorA(0.0f, 1.0f, 0.0f, 0.6f)); break;
-	case 2: gl::color(ColorA(0.0f, 0.0f, 1.0f, 0.6f)); break;
-	case 3: gl::color(ColorA(1.0f, 1.0f, 0.0f, 0.6f)); break;
-	case 4: gl::color(ColorA(0.0f, 1.0f, 1.0f, 0.6f)); break;
-	case 5: gl::color(ColorA(1.0f, 0.0f, 1.0f, 0.6f)); break;
-	}
-	gl::drawSolidCircle( cursor.getPos() * Vec2f( app::getWindowSize() ), 30 );
-}
-
-void draw2b( tuio::Cursor cursor )
-{
-	gl::color(ColorA(1.0f, 1.0f, 1.0f, 1.0f));
-	gl::drawSolidCircle( cursor.getPos() * Vec2f( app::getWindowSize() ), 5.0f );
-}
-
-void draw25d( tuio::Cursor25d cursor )
-{
-	gl::color(ColorA(0.0f, 1.0f, 0.0f, 1.0f));
-	float radius = 75.0f * cursor.getPos25().z;
-	gl::drawSolidCircle( cursor.getPos() * Vec2f( app::getWindowSize() ), radius );
-}
-
-void TuioClientApp::draw()
-{
-	gl::enableDepthRead();
-	gl::enableDepthWrite();	
-	gl::enableAlphaBlending();
-	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
-	
-	gl::setMatricesWindow( getWindowSize() );
-	if( tuio.isConnected() ) {
-
-		// Draw a center dot in all the cursors, to test the ability
-		// of tuio.getCursors() to get all cursors in one vector.
-		vector<tuio::Cursor> cursors = tuio.getCursors();
-		for( vector<tuio::Cursor>::const_iterator cursor = cursors.begin(); cursor != cursors.end(); ++cursor ) {
-			draw2b( *cursor );
-		}
-
-		// Draw each source's cursors in a different color, to test the ability
-		// of tuio.getCursors() to get each source's cursors independently.
-		std::set<std::string> sources = tuio.getSources();
-		int sourcenum = 0;
-		for( std::set<std::string>::const_iterator source = sources.begin(); source != sources.end(); ++source,++sourcenum ) {
-			vector<tuio::Cursor> cursors = tuio.getCursors(*source);
-			for( vector<tuio::Cursor>::const_iterator cursor = cursors.begin(); cursor != cursors.end(); ++cursor ) {
-				draw2( *cursor, sourcenum );
-			}
-		}
-
-		// If there any 25d cursors, draw them with radius proportional to the z value
-		vector<tuio::Cursor25d> cursors25d = tuio.getCursors25d();
-		for( vector<tuio::Cursor25d>::const_iterator cursor25d = cursors25d.begin(); cursor25d != cursors25d.end(); ++cursor25d ) {
-			draw25d( *cursor25d );
-		}
-	}
-}
-
-// This line tells Cinder to actually create the application
-CINDER_APP_BASIC( TuioClientApp, RendererGl )
